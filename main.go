41558da0afa775d22ee17c24e52b904e8396ec04
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2025-08-01 11:06:36 +0200

    Update current feature

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/fjammes/midi2osc/resources"
 	"github.com/hypebeast/go-osc/osc"
 	"github.com/xthexder/go-jack"
 	"gopkg.in/yaml.v3"
@@ -30,10 +31,18 @@ type Config struct {
 	Mappings  []Mapping `yaml:"mappings"`
 }
 
+type MidiEvent struct {
+	CC      uint8
+	Value   uint8
+	Target  string
+	Actions []OSCAction
+}
+
 var (
-	portIn *jack.Port
-	ch     chan string // for printing midi events
-	cfg    *Config
+	portIn    *jack.Port
+	ch        chan string // for printing midi events
+	cfg       *Config
+	eventChan chan MidiEvent // global channel for OSC events
 )
 
 func loadConfig(path string) (*Config, error) {
@@ -86,52 +95,69 @@ func process(nframes uint32) int {
 	events := portIn.GetMidiEvents(nframes)
 
 	if cfg == nil {
-		slog.Error("Skipping processing: config is nil")
+		// Ne pas logger ici pour ne pas bloquer JACK
 		return 0
 	}
 
 	for _, event := range events {
+		// Ne jamais bloquer dans le thread JACK :
+		select {
+		case ch <- fmt.Sprintf("%#v", event):
+		default:
+			// Si le chan est plein, on saute sans bloquer
+		}
 
-		slog.Debug("Received MIDI events", "count", len(events))
-		ch <- fmt.Sprintf("%#v", event)
 		if event.Buffer[0]&0xF0 == 0xB0 { // CC
 			cc := event.Buffer[1]
 			val := event.Buffer[2]
 
 			for _, m := range cfg.Mappings {
 				if m.CC == cc && m.Value == val {
-					slog.Info("MIDI CC matched", slog.Int("cc", int(cc)), slog.Int("value", int(val)))
-					for _, act := range m.Actions {
-						if err := sendOSC(cfg.OscTarget, act.Path, act.Type, act.Value); err != nil {
-							slog.Error("Failed to send OSC", slog.String("path", act.Path), slog.Any("err", err))
-						} else {
-							slog.Info("OSC sent", slog.String("path", act.Path), slog.Any("val", act.Value))
-						}
+					// Préparer une action à exécuter en dehors du thread JACK
+					msg := MidiEvent{
+						CC:      cc,
+						Value:   val,
+						Target:  cfg.OscTarget,
+						Actions: m.Actions,
+					}
+
+					select {
+					case eventChan <- msg:
+					default:
+						// Si le chan est plein, on ignore pour préserver le temps réel
 					}
 				}
 			}
 		}
 	}
-
 	return 0
 }
 
 func main() {
-	ch = make(chan string, 30)
-
-	cfgPath := flag.String("config", "mapping.yaml", "Path to YAML config")
-	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	slog.SetDefault(logger)
 
 	var err error
-	cfg, err = loadConfig(*cfgPath)
-	if err != nil {
-		slog.Error("Failed to load config", slog.String("file", *cfgPath), slog.Any("err", err))
-		os.Exit(1)
+	cfgPath := flag.String("config", "", "Path to YAML config")
+	flag.Parse()
+
+	if *cfgPath == "" {
+		var cfg Config
+		err := yaml.Unmarshal([]byte(resources.MidiMappingYaml), &cfg)
+		if err != nil {
+			slog.Error("Failed to parse embedded config", slog.Any("err", err))
+			os.Exit(1)
+		}
+		slog.Info("Loaded embedded config", slog.String("osc_target", cfg.OscTarget))
+	} else {
+		cfg, err = loadConfig(*cfgPath)
+		if err != nil {
+			slog.Error("Failed to load config", slog.String("file", *cfgPath), slog.Any("err", err))
+			os.Exit(1)
+		}
+		slog.Info("Loaded config", slog.String("osc_target", cfg.OscTarget))
 	}
-	slog.Info("Loaded config", slog.String("osc_target", cfg.OscTarget))
 
 	client, status := jack.ClientOpen("midi2osc", jack.NoStartServer)
 	if client == nil || status != 0 {
@@ -143,9 +169,30 @@ func main() {
 	if portIn == nil {
 		log.Fatal("Failed to register MIDI input port")
 	}
+	slog.Info("Registered MIDI input port", slog.String("name", portIn.GetName()))
+
+	eventChan = make(chan MidiEvent, 64) // global
+	ch = make(chan string, 64)
+	go func() {
+		for line := range ch {
+			slog.Debug("Raw MIDI", "event", line)
+		}
+	}()
+	go func() {
+		for msg := range eventChan {
+			for _, act := range msg.Actions {
+				err := sendOSC(msg.Target, act.Path, act.Type, act.Value)
+				if err != nil {
+					slog.Error("Failed to send OSC", slog.String("path", act.Path), slog.Any("err", err))
+				} else {
+					slog.Info("OSC sent", slog.String("path", act.Path), slog.Any("val", act.Value))
+				}
+			}
+		}
+	}()
 
 	if code := client.SetProcessCallback(process); code != 0 {
-		log.Fatalf("Failed to set process callback: ", jack.StrError(code))
+		slog.Error("Failed to set process callback:", slog.Any("err", jack.StrError(code)))
 		return
 	}
 	client.OnShutdown(func() {
@@ -153,7 +200,7 @@ func main() {
 	})
 
 	if code := client.Activate(); code != 0 {
-		fmt.Println("Failed to activate client: ", jack.StrError(code))
+		slog.Error("Failed to activate JACK client", slog.Any("err", jack.StrError(code)))
 		return
 	}
 	slog.Info("JACK client active", slog.String("name", client.GetName()))
---
+++ resources/constants.go
@@ -0,0 +1,8 @@
+package resources
+
+import (
+	_ "embed"
+)
+
+//go:embed midimapping.yaml
+var MidiMappingYaml string
